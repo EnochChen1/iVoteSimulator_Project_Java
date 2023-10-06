fe231c5873d96bbd079a0601dcc4c22c53ecee42
EnochChen1 <enoch.chen.personal@gmail.com>
2023-10-05 21:24:46 -0700

    Added comments to MultipleChoice and SingleChoice

--- iVoteSimulator/src/iVoteSimulator/MultipleChoice.java
+++ iVoteSimulator/src/iVoteSimulator/MultipleChoice.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 
 public class MultipleChoice implements Question {
 	private ArrayList<String> answerBank = new ArrayList<String>();
-	public void configureCandidateAnswer() { //I made the answer banks for the two classes different
-			answerBank.add("A"); //So that it would be more obvious what it is
+	public void configureCandidateAnswer() { 
+		//I made the answer banks for the two classes different
+		//So that it would be more obvious what it is
+			answerBank.add("A"); 
 			answerBank.add("B");
 			answerBank.add("C");
 			answerBank.add("D");
--- iVoteSimulator/src/iVoteSimulator/SingleChoice.java
+++ iVoteSimulator/src/iVoteSimulator/SingleChoice.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public class SingleChoice implements Question {
 	private ArrayList<String> answerBank = new ArrayList<String>();
 	public void configureCandidateAnswer() {
+		//answerBank for Single Choice is 1.Right and 2. Wrong,
+		// so that the difference would be more obvious
 			answerBank.add("1. Right");
 			answerBank.add("2. Wrong");
 	}
